4d61d16f7c3d3a11cb6e8579100c85ab73abfac0
Jake Felice <jasonfelice0@gmail.com>
2022-10-07 03:00:48 -0700

    Reset reducers

--- src/components/redux/configureStore.js
+++ src/components/redux/configureStore.js
@@ -9,7 +9,6 @@ const rootReducer = combineReducers({
 const store = configureStore({
   reducer: rootReducer,
   middleware: [applyMiddleware(thunk)],
-//   middleware: [thunk],
 });
 
 export const useStore = () => store;
