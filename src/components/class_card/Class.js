e69b1c99f0e9c30c6add01ef1cfbcf36f825fff8
Jake Felice <jasonfelice0@gmail.com>
2022-10-07 09:22:05 -0700

    Add social icons

--- src/components/class_card/Class.js
+++ src/components/class_card/Class.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import styles from './Class.module.css';
 import image from '../../tmp_assets/boxing.jpg';
@@ -10,6 +11,17 @@ export default function Class() {
         <h5>BOXING</h5>
         <span className={styles.seperator} />
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+      <div className={styles.socialIcons}>
+        <a className="social_icon twitter" href="https://facebook.com/">
+          <span />
+        </a>
+        <a className="social_icon facebook" href="https://twitter.com/">
+          <span />
+        </a>
+        <a className="social_icon instagram" href="https://instagram.com/">
+          <span />
+        </a>
       </div>
     </div>
   );
