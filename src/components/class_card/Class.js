9a3d6d1309874688289f39537f1f7d0484d3fc62
Jake Felice <jasonfelice0@gmail.com>
2022-10-07 08:29:10 -0700

    Create class card

--- src/components/class_card/Class.js
+++ src/components/class_card/Class.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from './Class.module.css';
+import image from '../../tmp_assets/boxing.jpg';
 
 export default function Class() {
   return (
-    <div className={styles.class}>Class</div>
+    <div className={styles.class}>
+      <img src={image} alt="" />
+      <div className={styles.classInfo}>
+        <h5>BOXING</h5>
+        <span className={styles.seperator} />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+    </div>
   );
 }
