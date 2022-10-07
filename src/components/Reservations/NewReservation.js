9624ec78db04a1ac57d93af4a350ef34f31e2f7c
bizip <bizip04@gmail.com>
2022-10-07 10:48:32 +0200

    Fix errors

--- src/components/Reservations/NewReservation.js
+++ src/components/Reservations/NewReservation.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReservationForm from '../forms/ReservationForm';
 
 const NewReservation = () => {
-    const handleForm =(formData)=>{
-        console.log(formData, "you made it")
-    }
-return <div>
-<ReservationForm handleForm = {handleForm} />
-</div>
+  const handleForm = (formData) => {
+    console.log(formData, 'you made it');
+  };
+  return (
+    <div>
+      <ReservationForm handleForm={handleForm} />
+    </div>
+  );
 };
 
 export default NewReservation;
--- src/components/forms/ReservationForm.js
+++ src/components/forms/ReservationForm.js
@@ -1,40 +1,43 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
 const ReservationForm = () => {
+  const [newDate, setNewDate] = useState({
+    date: '',
+    user_id: 0,
+    group_id: 0,
+  });
 
+  const handleChange = (e) => {
+    console.log(e);
+  };
 
-    const [newDate, setNewDate] = useState({
-        date: "",
-        user_id: 0,
-        group_id,
-    });
+  const handleSubmit = () => {
+    console.log(newDate);
+  };
 
-    const handleChange = (e) => {
-        console.log(e)
-    }
-
-    const handleSubmit = () => {
-        console.log(newDate)
-    }
-
-    return <form onSubmit={handleSubmit}>
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label htmlFor="date">Date</label>
-                <input type="date" id='date'
-                    value={newDate}
-                    min="2020-01-01"
-                    max="2023-01-01"
-                    onChange={(e) => setNewDate({
-                        ...newDate,
-                        date: e.target.value
-                    })}
-                />
-            </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label htmlFor="date">Date</label>
+          <input
+            type="date"
+            id="date"
+            value={newDate}
+            min="2020-01-01"
+            max="2023-01-01"
+            onChange={(e) => setNewDate({
+              ...newDate,
+              date: e.target.value,
+            })}
+          />
         </div>
-        <div className="new-expense-actions">
-            <button type="submit">Reserve</button>
-        </div>
+      </div>
+      <div className="new-expense-actions">
+        <button type="submit">Reserve</button>
+      </div>
     </form>
+  );
 };
 
 export default ReservationForm;
