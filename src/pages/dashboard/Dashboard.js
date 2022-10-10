94e8dbb5ea4f063622d68b4612236d47a7af0ab4
Jake Felice <jasonfelice0@gmail.com>
2022-10-10 11:48:58 -0700

    Pass fetch classes on dashbaord

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './Dashboard.module.css';
 import Class from '../../components/class_card/Class';
+import { fetchClasses } from '../../redux/classes/classes';
 
 function DashBoard() {
+  const dispatch = useDispatch();
+  const classes = useSelector((state) => state.class.classes);
+
+  useEffect(() => {
+    dispatch(fetchClasses());
+  }, [dispatch]);
+
   return (
     <>
       <main>
@@ -12,7 +21,15 @@ function DashBoard() {
           <span className={styles.seperator} />
         </div>
         <div className={styles.classList}>
-          <Class />
+          {classes.map((group) => (
+            <Class
+              key={group.id}
+              id={group.id}
+              title={group.title}
+              image={group.image}
+              description={group.description}
+            />
+          ))}
         </div>
       </main>
     </>
