ef8653cbc1e9b3576d62db06c7313bc37fab410e
Jake Felice <jasonfelice0@gmail.com>
2022-10-10 11:20:45 -0700

    Configured classes reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import updateSession from './authentication/authentication';
+import updateClasses from './classes/classes';
 
 const store = configureStore({
   reducer: {
     session: updateSession,
+    class: updateClasses,
   },
 });
 
