33dba300998ca0e739e1f803b9ae2deb2e79c800
Jake Felice <jasonfelice0@gmail.com>
2022-10-10 11:50:09 -0700

    Pass props to class cards

--- src/components/class_card/Class.js
+++ src/components/class_card/Class.js
@@ -1,16 +1,21 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import styles from './Class.module.css';
-import image from '../../tmp_assets/boxing.jpg';
 
-export default function Class() {
+export default function Class(props) {
+  const {
+    id, title, image, description,
+  } = props;
   return (
     <div className={styles.class}>
-      <img src={image} alt="" />
+      <Link to={`/class/${id}`}>
+        <img src={image} alt="" />
+      </Link>
       <div className={styles.classInfo}>
-        <h5>BOXING</h5>
+        <h5>{title}</h5>
         <span className={styles.seperator} />
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <p>{description}</p>
       </div>
       <div className={styles.socialIcons}>
         <a className="social_icon twitter" href="https://facebook.com/">
@@ -26,3 +31,10 @@ export default function Class() {
     </div>
   );
 }
+
+Class.propTypes = {
+  id: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
