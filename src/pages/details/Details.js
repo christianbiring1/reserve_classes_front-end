5a3346a37523829966de12bfb4d4598adca06a91
Jake Felice <jasonfelice0@gmail.com>
2022-10-10 11:20:05 -0700

    Create and design class deatails page

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, Link } from 'react-router-dom';
 import styles from './Details.module.css';
-import image from '../../tmp_assets/boxing.jpg';
+import { fetchDetails } from '../../redux/classes/classes';
 
 export default function Details() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchDetails(id));
+  }, []);
+
+  const classDetails = useSelector((state) => state.class.details);
+
   return (
     <section className={styles.main}>
-      <div className={styles.image}>
-        <img src={image} alt="" />
-        <div className={styles.backButton}>
-          <span />
-        </div>
-      </div>
-      <div className={styles.details}>
-        <div className={styles.info}>
-          <h1>Title</h1>
-          <p>here goes description</p>
-        </div>
-        <ul className={styles.infoList}>
-          <li>Rating</li>
-          <li>Location</li>
-        </ul>
-        <button type="button">Reserve</button>
-      </div>
+      {classDetails.id && (
+        <>
+          <div className={styles.image}>
+            <img src={classDetails.image} alt="" />
+            <Link to="/" className={styles.backButton}>
+              <span />
+            </Link>
+          </div>
+          <div className={styles.details}>
+            <div className={styles.info}>
+              <h1>{classDetails.title}</h1>
+              <p>{classDetails.description}</p>
+            </div>
+            <ul className={styles.infoList}>
+              <li>
+                <span>Rating: </span>
+                <span>{classDetails.rating}</span>
+              </li>
+            </ul>
+            <button type="button">Reserve</button>
+          </div>
+        </>
+      )}
+
+      {!classDetails.id && (
+        <>
+          <div className={styles.error}>
+            <h1>Error 404</h1>
+            <h3>Class not found!</h3>
+            <p>
+              Please select one from
+              {' '}
+              <Link to="/">the list</Link>
+              .
+            </p>
+          </div>
+        </>
+      )}
+
     </section>
   );
 }
