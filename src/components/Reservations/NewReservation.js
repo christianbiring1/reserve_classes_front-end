8a4f5ac95b3bdd0b7ca9c76a3e1ed03de20c7a77
christian <christianbiringanine22@gmail.com>
2022-10-11 13:23:07 +0200

    Add panel to the reservation

--- src/components/Reservations/NewReservation.js
+++ src/components/Reservations/NewReservation.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import ReservationForm from '../forms/ReservationForm';
 import { newreservation } from '../../redux/newReservationSlice';
+import Navbar from '../Navbar';
 
 const NewReservation = () => {
   const dispatch = useDispatch();
@@ -12,6 +13,9 @@ const NewReservation = () => {
   };
   return (
     <div>
+      <div className="nav_panel">
+        <Navbar />
+      </div>
       <ReservationForm handleForm={handleForm} />
     </div>
   );
