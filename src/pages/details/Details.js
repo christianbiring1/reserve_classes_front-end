8af326b0bd7bbfa712c562062b08f80fbae6f1a0
Jake Felice <jasonfelice0@gmail.com>
2022-10-12 01:55:43 -0700

    Add loading ripple in details page

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -3,20 +3,26 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
 import styles from './Details.module.css';
 import { fetchDetails } from '../../redux/classes/classes';
+import { loading, loaded } from '../../redux/loading/loading';
+import ripple from '../../assets/loading_ripple.svg';
 
 export default function Details() {
   const dispatch = useDispatch();
   const { id } = useParams();
 
   useEffect(() => {
-    dispatch(fetchDetails(id));
+    dispatch(loading());
+    dispatch(fetchDetails(id)).then(() => {
+      dispatch(loaded());
+    });
   }, []);
 
   const classDetails = useSelector((state) => state.class.details);
+  const isLoading = useSelector((state) => state.loading);
 
   return (
     <section className={styles.main}>
-      {classDetails.id && (
+      {(classDetails.id && !isLoading) && (
         <>
           <div className={styles.image}>
             <img src={classDetails.image} alt="" />
@@ -40,7 +46,7 @@ export default function Details() {
         </>
       )}
 
-      {!classDetails.id && (
+      {(!classDetails.id && !isLoading) && (
         <>
           <div className={styles.error}>
             <h1>Error 404</h1>
@@ -53,6 +59,10 @@ export default function Details() {
             </p>
           </div>
         </>
+      )}
+
+      {isLoading && (
+        <img className={styles.ripple} src={ripple} alt="" />
       )}
 
     </section>
